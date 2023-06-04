fef005883f68454b911d156690dd052d483f4b75
Bruno Fernandes (up202108871) <up202108871@up.pt>
2023-06-04 15:59:18 +0100

    Reran doxygen

--- Documentation/html/search/all_8.js
+++ Documentation/html/search/all_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['linkernighan_0',['linKernighan',['../class_controller.html#aca66403a6d7a56fd6682e35fd1aca940',1,'Controller']]]
+  ['linkernighan_0',['linKernighan',['../class_controller.html#aca66403a6d7a56fd6682e35fd1aca940',1,'Controller']]],
+  ['lk_1',['LK',['../class_controller.html#a56567dd3acf2871bb9e60b3ba01893f6',1,'Controller']]]
 ];
